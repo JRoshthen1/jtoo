fc857c56e75761f2eb830ec4641ce0aa67268be7
Martin Jaros <jroshthen1@gmail.com>
2022-12-03 21:21:26 +0200

    Adding quotable api

--- greeting.js
+++ greeting.js
@@ -48,5 +48,5 @@ function displayClock() {
 	document.getElementById('month').innerText = mm;
 	document.getElementById('day').innerText = dd;
 
-	setTimeout(displayClock, 1000);
+	setTimeout(displayClock, 60000);
 }
